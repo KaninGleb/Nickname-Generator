215f8bdc4435af8e80cc690b3b82a33ab770d08d
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-01 00:02:07 +0300

    getRandomFormat function added, generateNickname function supplemented

--- scripts.js
+++ scripts.js
@@ -1,11 +1,35 @@
 const prefixes = ["Cool", "Wild", "Sneaky", "Happy", "Lucky", "Crazy", "Swift"];
 const suffixes = ["Panda", "Ninja", "Tiger", "Wizard", "Dragon", "Master", "Queen"];
 
+
+function getRandomFormat() {
+    let formatType = Math.floor(Math.random() * 4);
+    let randomDigits = Math.floor(Math.random() * 100);
+    const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "+"];
+    let randomSymbol = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+    switch (formatType) {
+        case 0:
+            return ' ';
+        case 1:
+            return '_';
+        case 2:
+            return randomDigits + randomSymbol;
+        default:
+            return '';
+    }
+}
+
+
 function generateNickname() {
     const prefix = prefixes[Math.floor(Math.random() * prefixes.length)];
     const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
-    return `${prefix} ${suffix}`;
+    const useRandomFormat = document.getElementById('toggle-format').checked;
+
+    const separator = useRandomFormat ? getRandomFormat() : ' ';
+    return prefix + separator + suffix;
 }
+
 
 function setupEventListeners() {
     const button = document.getElementById('generate-btn');
