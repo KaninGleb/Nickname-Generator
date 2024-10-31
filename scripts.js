2b7f5ec639a42dcae7598dcfcb6820df48a16643
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-31 23:39:20 +0300

    generateNickname function added

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 const prefixes = ["Cool", "Wild", "Sneaky", "Happy", "Lucky", "Crazy", "Swift"];
 const suffixes = ["Panda", "Ninja", "Tiger", "Wizard", "Dragon", "Master", "Queen"];
+
+function generateNickname() {
+    const prefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+    const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
+    return `${prefix} ${suffix}`;
+}
+
+console.log(generateNickname());
